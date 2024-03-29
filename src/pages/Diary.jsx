0c136844925e0c3171279e40bafe3b3a87e69818
Hyunjun-11 <mical0108@gmail.com>
2024-03-29 14:35:07 +0900

    public & assets imgFile load

--- src/pages/Diary.jsx
+++ src/pages/Diary.jsx
@@ -2,7 +2,7 @@ import { useParams } from "react-router-dom";
 
 const Diary = () => {
     const params = useParams();
-    return <div>Diary</div>
+    return <div>{params.id}번 일기입니다.</div>
 }
 
 export default Diary;
